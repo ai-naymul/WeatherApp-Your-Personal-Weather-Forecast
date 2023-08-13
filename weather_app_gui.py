8b774eb1dc01db6f60c2c7e8cb83ffe80713b391
Naymul Islam <naymul504@gmail.com>
2023-08-13 20:23:41 +0600

    time label added
    time label in the gui is added

--- weather_app_gui.py
+++ weather_app_gui.py
@@ -7,6 +7,7 @@ tk.config(padx=20,pady=10)
 
 city_label = Label(text="Enter The City Name: ")
 city_label.grid(row=0, column=0)
+
 city_entry = Entry()
 city_entry.grid(row=0, column=1)
 
@@ -17,16 +18,18 @@ weather_label_frame = LabelFrame(text="Weather Info")
 weather_label_frame.grid(row=1,column=0, columnspan=3, padx=20, pady=20, sticky="ew")
 
 
-temp = Label(weather_label_frame,text="Tempareture: ")
+temp = Label(weather_label_frame,text="Temperature: ")
 temp.grid(row=0, column=0, padx=10, pady=5, sticky="w")
 
 
 humidity = Label(weather_label_frame,text='Humidity: ')
 humidity.grid(row=1,column=0,pady=5,padx=10,sticky='w')
 
+time = Label(weather_label_frame,text="Time: ")
+time.grid(row=2,column=0,pady=5,padx=10,sticky='w')
 
 condition = Label(weather_label_frame,text="Condition: ")
-condition.grid(row=2,column=0,padx=10,pady=5,sticky='w')
+condition.grid(row=3,column=0,padx=10,pady=5,sticky='w')
 
 
 tk.mainloop()
